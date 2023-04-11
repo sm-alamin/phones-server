17ad0a5d0f33c9e14bfd2e0056e73ca63f4815d6
sm-alamin <sma.fkml@gmail.com>
2023-04-11 21:27:17 +0600

    learning of first server is done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors =require('cors');
+const phones = require('./phones.json');
+const app = express();
+const port = 5000;
+app.use(cors());
+
+app.get('/', (req, res)=>{
+    res.send('my phone information is coming soon toon');
+})
+app.get('/phones', (req,res)=>{
+    res.send(phones)
+})
+app.get('/phones/:id', (req,res)=>{
+    const id = parseInt(req.params.id)
+    console.log(`I need data from ${id}`)
+    const phone = phones.find(phone => phone.id === id) || {}
+    res.send(phone)
+})
+
+app.listen(port, () =>{
+    console.log(`my server in running on ${port}`)
+})
